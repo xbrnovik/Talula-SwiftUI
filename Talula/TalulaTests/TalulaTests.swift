8c1d8dff83fa03105774ea27b8f5ea57f486f9ad
xbrnovik <xbrnovik@node.mendelu.cz>
2020-02-14 16:17:33 +0100

    Added unit test

--- Talula/TalulaTests/TalulaTests.swift
+++ Talula/TalulaTests/TalulaTests.swift
@@ -2,33 +2,52 @@
 //  TalulaTests.swift
 //  TalulaTests
 //
-//  Created by Diana Brnovik on 13/02/2020.
-//  Copyright © 2020 Diana Brnovik. All rights reserved.
+//  Created by Diana Brnovik on 30/01/2019.
+//  Copyright © 2019 Diana Brnovik. All rights reserved.
 //
 
 import XCTest
 @testable import Talula
+import CoreData
 
 class TalulaTests: XCTestCase {
+    
+    var storage: MeteoriteStorage?
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        storage = MeteoriteStorage()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testGetById() {
+        // Properties.
+        let meteoriteId: String = "1"
+        let name: String = "Test meteorite"
+        let geotype: String = "A1"
+        let latitude: Double = 1
+        let longitude: Double = 1
+        let mass: Float = 10
+        // New meteorite.
+        let meteorite = storage?.create()
+        meteorite?.meteoriteId = meteoriteId
+        meteorite?.name = name
+        meteorite?.geotype = geotype
+        meteorite?.latitude = latitude
+        meteorite?.longitude = longitude
+        meteorite?.mass = mass
+        // Call to function.
+        let savedMeteorite = storage?.getById(id: "1")
+        // Test.
+        XCTAssertEqual(savedMeteorite?.meteoriteId, meteoriteId)
+        XCTAssertEqual(savedMeteorite?.name, name)
+        XCTAssertEqual(savedMeteorite?.geotype, geotype)
+        XCTAssertEqual(savedMeteorite?.latitude, latitude)
+        XCTAssertEqual(savedMeteorite?.longitude, longitude)
+        XCTAssertEqual(savedMeteorite?.mass, mass)
     }
 
 }
